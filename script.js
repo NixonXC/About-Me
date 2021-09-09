d25928bd85f475828a389006d89296080e646f5d
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-09-09 17:08:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("b4").onclick = function ()
+    {
+     window.location.href="https://github.com/nixonxc";
+    }
+
+document.getElementById("b5").onclick = function ()
+{
+    window.location.href="https://www.youtube.com/channel/UCcHYLtjKzgfSyeLxSyDMaKQ";
+}
+console.error("This is a fake error");
+document.getElementById("b8").onclick = function ()
+{
+    window.location.href="https://mars.nasa.gov/";
+}
